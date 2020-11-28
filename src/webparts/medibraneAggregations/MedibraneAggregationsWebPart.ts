f31fbd54f999a47ccf1a803159a50835407d4fae
bresleveloper <ariel.rubi@gmail.com>
2020-11-28 20:50:42 +0200

    i advanced like thig

--- src/webparts/medibraneAggregations/MedibraneAggregationsWebPart.ts
+++ src/webparts/medibraneAggregations/MedibraneAggregationsWebPart.ts
@@ -23,14 +23,39 @@ export interface IMedibraneAggregationsWebPartProps {
 export default class MedibraneAggregationsWebPart extends BaseClientSideWebPart<IMedibraneAggregationsWebPartProps> {
 
   public render(): void {
+/*
+    this.getListItems('Quotes').then((items)=>{});
+    this.getListItems('Orders').then((items)=>{});
+    this.getListItems('Projects').then((items)=>{});
+*/
 
-    this.getListItems('Quotas').then((data)=>{
-      console.log('quotas', data);
-    });
+    this.getListItemsShhh('Quotes');
+    this.getListItemsShhh('Orders');
+    this.getListItemsShhh('Projects');
 
-    this.getListItems('Quotes').then((data)=>{
-      console.log('quotes', data);
-    });
+    
+  }
+
+  ajaxCounter:number = 0;
+  listsContainer:{} = {};
+  
+  public buildHtml(){
+    let quotesTotal = 0;
+    let oredersTotal = 0;
+    let projTotal = 0;
+
+    let forche = (arr:[], fName:string) => {
+      let t = 0
+      for (let i = 0; i < arr.length; i++) {
+        const item = arr[i];
+        t += item[fName]
+      }
+      return t;
+    }
+
+    quotesTotal = forche(this.listsContainer['Quotes'], 'Quota_x0020_amount')
+    oredersTotal = forche(this.listsContainer['Orders'], 'Order_x0020_Amount')
+    projTotal = forche(this.listsContainer['Projects'], 'Order_x0020_Amount')
 
 
 
@@ -45,10 +70,16 @@ export default class MedibraneAggregationsWebPart extends BaseClientSideWebPart<
               <a href="https://aka.ms/spfx" class="${ styles.button }">
                 <span class="${ styles.label }">Learn more</span>
               </a>
+
+              <h2>quotes :  ${quotesTotal}</h2>
+              <h2>orders :  ${oredersTotal}</h2>
+              <h2>projs :  ${projTotal}</h2>
+
             </div>
           </div>
         </div>
       </div>`;
+
   }
 
   protected get dataVersion(): Version {
@@ -78,14 +109,43 @@ export default class MedibraneAggregationsWebPart extends BaseClientSideWebPart<
   }
 
 
-  public getListItems(listname:string): Promise<any> {
+  public getListItems(listname:string): Promise<{}[]> {
+    console.log('asking list items for', listname);
+    this.ajaxCounter++;
+
     return this.context.spHttpClient.get(
       this.context.pageContext.web.absoluteUrl + 
       `/_api/web/lists/GetByTitle('${listname}')/Items`, SPHttpClient.configurations.v1)
           .then((response: SPHttpClientResponse) => {
-              return response.json();
+              let items = response.json()['value'];
+              console.log('list items for', listname, items);
+              this.ajaxCounter--;
+              if (this.ajaxCounter == 0) {
+                
+              }
+              return items;
           });
     }
 
+    public getListItemsShhh(listname:string): void {
+      console.log('asking list items for', listname);
+      this.ajaxCounter++;
+  
+      this.context.spHttpClient.get(
+        this.context.pageContext.web.absoluteUrl + 
+        `/_api/web/lists/GetByTitle('${listname}')/Items`, SPHttpClient.configurations.v1)
+            .then((response: SPHttpClientResponse) => {
+                response.json().then((data)=> {
+                  
+                    console.log('list items for', listname, data);
+                    this.ajaxCounter--;
+                    this.listsContainer[listname] = data.value;
+                    if (this.ajaxCounter == 0) {
+                      this.buildHtml();
+                    }
+
+                });
+            });
+      }
 
 }
